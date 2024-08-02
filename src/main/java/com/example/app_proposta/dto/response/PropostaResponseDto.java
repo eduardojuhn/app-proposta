b00e9704b21cebaad1c3fbf3a902c2bf33cf97de
eduardojuhn <dudu.juhn.02@gmail.com>
2024-08-01 22:07:02 -0300

    feat: mapeando campo valor solicitado

--- src/main/java/com/example/app_proposta/dto/response/PropostaResponseDto.java
+++ src/main/java/com/example/app_proposta/dto/response/PropostaResponseDto.java
@@ -2,5 +2,5 @@ package com.example.app_proposta.dto.response;
 
 public record PropostaResponseDto(Long id,
                                   String nome, String sobrenome, String telefone, String cpf,
-                                  Double renda, Double valorSolicitado,
+                                  Double renda, String valorSolicitadoFmt,
                                   int prazoPagamento) {}
