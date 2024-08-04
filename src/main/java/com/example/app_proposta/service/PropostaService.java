25e7c7a1a13cd98000b38f13551dc8aa76778c6b
eduardojuhn <dudu.juhn.02@gmail.com>
2024-08-03 23:46:25 -0300

    feat: pegando o valor do exchange do properties

--- src/main/java/com/example/app_proposta/service/PropostaService.java
+++ src/main/java/com/example/app_proposta/service/PropostaService.java
@@ -6,6 +6,7 @@ import com.example.app_proposta.entity.Proposta;
 import com.example.app_proposta.mapper.PropostaMapper;
 import com.example.app_proposta.repository.PropostaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +18,15 @@ public class PropostaService {
     private final PropostaRepository propostaRepository;
     private final NotificacaoService notificacaoService;
 
+    @Value("${rabbitmq.propostapendente.exchange}")
+    private String exchange;
+
     public PropostaResponseDto criar(PropostaRequestDto requestDto) {
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
         propostaRepository.save(proposta);
 
         PropostaResponseDto responseDto = PropostaMapper.INSTANCE.convertEntitytoDto(proposta);
-        notificacaoService.notificar(responseDto, "proposta-pendente.ex");
+        notificacaoService.notificar(responseDto, exchange);
         return PropostaMapper.INSTANCE.convertEntitytoDto(proposta);
     }
 
