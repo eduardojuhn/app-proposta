70d8937b70aae12098cc56893b28ecf383e443a8
eduardojuhn <dudu.juhn.02@gmail.com>
2024-08-10 23:50:50 -0300

    feat: findAllByIntegradaIsFalse

--- src/main/java/com/example/app_proposta/repository/PropostaRepository.java
+++ src/main/java/com/example/app_proposta/repository/PropostaRepository.java
@@ -3,5 +3,9 @@ package com.example.app_proposta.repository;
 import com.example.app_proposta.entity.Proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+
+    List<Proposta> findAllByIntegradaIsFalse();
 }
